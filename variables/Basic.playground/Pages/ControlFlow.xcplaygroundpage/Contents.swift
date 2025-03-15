2e1ce88f80fb2c8562a0da76e9a8f432bd4efc35
VaibhavVadle <vaibhavvadle09gmail.com>
2025-03-16 02:31:47 +0530

    Basic optional variables usage

--- variables/Basic.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
+++ variables/Basic.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -15,3 +15,27 @@ let scoreDecoration = if teamScore > 10 {
     ""
 }
 print("Score:", teamScore, scoreDecoration)
+
+// Optional or Null variable
+var optionalString: String? = "Hello!"
+print(optionalString == nil)
+
+var optionalName: String? = "John"
+var greeting = "Hello!"
+optionalName = nil
+if let name = optionalName {
+    greeting = "Hello, \(name)!"
+}else {
+    greeting = "Hello!"
+}
+print(greeting)
+
+let nickname: String? = nil
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickname ?? fullName)"
+
+if let nickname = nickname {
+    print("Hello, \(nickname)!")
+}
+
+
