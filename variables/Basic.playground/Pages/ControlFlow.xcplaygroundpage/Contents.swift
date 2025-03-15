4223fa54d03c4a5c126198ad0b6898620c63765e
VaibhavVadle <vaibhavvadle09gmail.com>
2025-03-16 03:10:56 +0530

    Basic switch cases, dictionaries, while & repeat-while loop and index in loop usage

--- variables/Basic.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
+++ variables/Basic.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -38,4 +38,54 @@ if let nickname = nickname {
     print("Hello, \(nickname)!")
 }
 
+// Switch case
+let vegetable = "red pepper"
+switch vegetable {
+    case "celery":
+        print("Add some raisins and make ants on a log.")
+    case "cucumber", "watercress":
+        print("That would make a good tea sandwich.")
+    case let x where x.hasSuffix("pepper"):
+        print("Is it a spicy \(x)?")
+    default:
+        print("Everything tastes good in soup.")
+}
 
+// Dictionaries
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+var largestName: String?
+for(name,numbers) in interestingNumbers{
+    for number in numbers{
+        if number > largest{
+            largest = number
+            largestName = name
+        }
+    }
+}
+print("\(largestName ?? "Unknown") - \(largest)")
+
+// while loop
+var n = 2
+while n < 2 {
+    n *= 2
+}
+print(n)
+
+// repeat-while loop
+var m = 2
+repeat {
+    m *= 2
+} while m < 2
+print(m)
+
+// ..< - used for range of index
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+print(total)
