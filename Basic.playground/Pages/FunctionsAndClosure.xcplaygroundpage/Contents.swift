58ab687362e0b84b2bb62b58decb999066ba9c8a
VaibhavVadle <vaibhavvadle09@gmail.com>
2025-03-18 23:50:56 +0530

    Added function declaration, no argument, tuples & nested function usage

--- Basic.playground/Pages/FunctionsAndClosure.xcplaygroundpage/Contents.swift
+++ Basic.playground/Pages/FunctionsAndClosure.xcplaygroundpage/Contents.swift
@@ -1,7 +1,44 @@
-//: [Previous](@previous)
+// Declare function
+func greet(person: String, day:String) -> String {
+    return "Hello, \(person)! Today is \(day)."
+}
+greet(person: "Alex", day: "Tuesday")
 
-import Foundation
+// _ used to for no argument label
+func greet(_ person: String, on day: String) -> String {
+    return "Hello, \(person)! Today is \(day)."
+}
+greet("Bob", on: "Wednesday")
 
-var greeting = "Hello, playground"
+// Tuple - make compound values & return multiple values from a function
+func calculateStatistics(scores:[Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    for score in scores {
+        if(score > min) {
+            max = score
+        } else {
+            min  = score
+        }
+        sum += score
+    }
+    return (min, max, sum)
+}
 
-//: [Next](@next)
+let statistics = calculateStatistics(scores: [10,20,3,119,49])
+print(statistics.min)
+print(statistics.max)
+print(statistics.sum)
+print(statistics.2)
+
+// Nested function
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+returnFifteen()
