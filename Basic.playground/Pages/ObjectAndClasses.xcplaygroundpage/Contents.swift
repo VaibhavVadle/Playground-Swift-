8a85b39f9f423af9e0f86104e4df3ad7fa1e4583
VaibhavVadle <vaibhavvadle09@gmail.com>
2025-03-19 23:58:33 +0530

    Init variable on instance create

--- Basic.playground/Pages/ObjectAndClasses.xcplaygroundpage/Contents.swift
+++ Basic.playground/Pages/ObjectAndClasses.xcplaygroundpage/Contents.swift
@@ -15,3 +15,19 @@ shape.numberOfSides = 10
 var shapeDescription = shape.simpleDescription()
 print(shapeDescription)
 shape.printValue("Hello world!")
+
+// init variable when an instance is created
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+
+    init(name: String) {
+       self.name = name
+    }
+
+    func simpleDescription() -> String {
+       return "A shape with \(numberOfSides) sides."
+    }
+}
+
+var namedShape = NamedShape(name: "Square")
