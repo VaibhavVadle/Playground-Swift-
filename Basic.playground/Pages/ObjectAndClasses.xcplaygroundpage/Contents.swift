0ea7bcf46fe891a91bda99a77c8727332858d4a0
VaibhavVadle <vaibhavvadle09@gmail.com>
2025-03-22 17:01:25 +0530

    willSet, didSet & optional values usage

--- Basic.playground/Pages/ObjectAndClasses.xcplaygroundpage/Contents.swift
+++ Basic.playground/Pages/ObjectAndClasses.xcplaygroundpage/Contents.swift
@@ -63,17 +63,15 @@ class EquilateralTriangle: NamedShape {
         super.init(name: name)
         numberOfSides = 3
     }
-
-
-    var perimeter: Double {
+    
+    var perimeter : Double {
         get {
-             return 3.0 * sideLength
+            return sideLength * 3
         }
         set {
-            sideLength = newValue / 3.0
+            sideLength = newValue / 3
         }
     }
-
 
     override func simpleDescription() -> String {
         return "An equilateral triangle with sides of length \(sideLength)."
@@ -81,7 +79,44 @@ class EquilateralTriangle: NamedShape {
 }
 var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
 print(triangle.perimeter)
-// Prints "9.3"
 triangle.perimeter = 9.9
 print(triangle.sideLength)
-// Prints "3.3000000000000003"
+
+// To compute property before use willSet and for after use didSet
+// Below class ensure triangle and square have same sideLength
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+        didSet {
+            print("Triangle updated -> \(triangle.sideLength)")
+        }
+    }
+    
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+        didSet {
+            print("Square updated -> \(square.sideLength)")
+        }
+    }
+    
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare =  TriangleAndSquare(size: 5, name: "Another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "New shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+
+// Optional values
+let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+let sideLength = optionalSquare?.sideLength
+
