718250ef4e9f46bba4e2ddcd6e69e75e1520e48c
VaibhavVadle <vaibhavvadle09@gmail.com>
2025-03-30 23:29:28 +0530

    Generate random values usage

---
+++ Basic.playground/Pages/RandomVariables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+// Close Range - includes 1 and 10
+let value = Int.random(in: 1...10)
+
+// Excludes 10
+let variable1 = Int.random(in: 1 ..< 10);
+
+let variable2 = Double.random(in: 1 ... 10)
+
+let variable3 = Float.random(in: 1 ..< 10)
+
+let variable4 = Bool.random()
